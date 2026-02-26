fe9b9037f6e6f6b475a137dec680832fec39e3c6
agentosroza-dev <agentosroza@gmail.com>
2026-02-26 17:34:03 +0700

    add vite front end

--- frontend/src/components/includes/FooterIn.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-inline">Anything you want</div>
-    <strong>Copyright © 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-  </footer>
-</template>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,33 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  server: {
-    host: '0.0.0.0',
-    // eslint-disable-next-line no-undef
-    port: process.env.VITE_HMR_PORT || 5173,
-    hmr: {
-      protocol: 'ws',
-      // eslint-disable-next-line no-undef
-      port: process.env.VITE_HMR_PORT || 5173,
-    },
-    watch: {
-      usePolling: true,
-      useFsEvents: true,
-      interval: 1000,
-    },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@com': fileURLToPath(new URL('./src/components', import.meta.url)),
-      '@func': fileURLToPath(new URL('./src/functions', import.meta.url)),
-      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
-    },
-  },
+  plugins: [vue()],
 })
